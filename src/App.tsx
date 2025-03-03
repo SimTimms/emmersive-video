02371e8df6d09fd8408d0c0b696c7a934cb60516
Tim Simms <tim-simms@hotmail.com>
2025-03-03 14:26:24 +0000

    push

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,50 @@
 import { Canvas } from "@react-three/fiber";
-import { useEffect } from "react";
+import { use, useEffect, useRef, useState } from "react";
 import Scene from "./Scene";
+import url from "./assets/car.mp4";
+import url2 from "./assets/car2.mp4";
+import * as THREE from "three";
 
 function App() {
-  useEffect(() => {});
+  const ref = useRef<HTMLVideoElement>(null);
+  const [videoState, setVideoState] = useState<HTMLVideoElement | null>(null);
+  const [path, setPath] = useState<any[] | null>(null);
+  const [sceneNbr, setSceneNbr] = useState<number>(0);
+
+  const path1 = [new THREE.Vector3(0.5, 0, 4), new THREE.Vector3(0.1, 0, 4)];
+  const path2 = [
+    new THREE.Vector3(1.0, 1.6, -3),
+    new THREE.Vector3(1.5, 1.6, -3),
+  ];
+
+  const paths = [path1, path2];
+  useEffect(() => {
+    if (ref.current) {
+      setVideoState(ref.current);
+    }
+  }, [videoState]);
+
+  useEffect(() => {
+    setPath(paths[sceneNbr]);
+    if (ref.current && sceneNbr > 0) {
+      ref.current.src = url2;
+    }
+  }, [sceneNbr]);
+
   return (
     <div className="background">
+      <video src={`${url}`} muted className="video-full" ref={ref} autoPlay />
       <Canvas
         className="canvas"
-        camera={{ position: [0, 1, 4], far: 1400 }}
+        camera={{ fov: 35, position: [0, 0, 4], far: 400 }}
         shadows
       >
-        <Scene />
+        <Scene
+          video={videoState}
+          path={path}
+          setSceneNbr={setSceneNbr}
+          sceneNbr={sceneNbr}
+        />
       </Canvas>
     </div>
   );
--- src/Scene.tsx
+++ src/Scene.tsx
@@ -1,5 +1,5 @@
 import { OrbitControls } from "@react-three/drei";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { GLTFLoader } from "three/examples/jsm/Addons.js";
 import { useLoader, useThree, useFrame } from "@react-three/fiber";
 import {
@@ -8,15 +8,25 @@ import {
   Bloom,
   Noise,
 } from "@react-three/postprocessing";
+import { Perf } from "r3f-perf";
 import { Environment } from "@react-three/drei";
 import * as THREE from "three";
 
-function Scene() {
-  const carModel = useLoader(GLTFLoader, "./models/car.glb");
+interface SceneProps {
+  video: HTMLVideoElement | null;
+  path: any[] | null;
+  setSceneNbr: any;
+  sceneNbr: number;
+}
+function Scene(props: SceneProps) {
+  const { video, path, setSceneNbr, sceneNbr } = props;
+
+  const carModel = useLoader(GLTFLoader, "./models/car-low.glb");
   const tunnelModel = useLoader(GLTFLoader, "./models/tunnel.glb");
   const camera = useThree((state: any) => state.camera);
   const headlightsRef = useRef<any>(null);
   const brakelightsRef = useRef<any>(null);
+
   carModel.scene.traverse(function (node) {
     if (node.name === "headlights") {
       headlightsRef.current = node;
@@ -24,8 +34,12 @@ function Scene() {
       node.material = new THREE.MeshStandardMaterial({
         color: 0x000000,
         emissive: 0xffffff,
-        emissiveIntensity: 0,
       });
+    }
+    if (node.name === "chassis" && node instanceof THREE.Mesh) {
+      node.material.color.set(0xf2a407);
+      node.material.roughness = 1.2;
+      node.material.metalness = 0.8;
     }
     if (node.name === "brakelight") {
       brakelightsRef.current = node;
@@ -48,7 +62,6 @@ function Scene() {
     }
   });
 
-  const animationTime = 10;
   const actions: { [key: number]: () => void } = {
     0: () => (headlightsRef.current.material.emissiveIntensity = 0),
     5: () => {
@@ -63,101 +76,85 @@ function Scene() {
     const elapsedTime = clock.getElapsedTime();
 
     // const frameDetails = frames[0].points[currentFrame];
-    const time = (elapsedTime * (1 / animationTime)) % 1;
-    const path = [
-      new THREE.Vector3(0, 1, 4),
-      new THREE.Vector3(-0.7, 1, 3),
-      new THREE.Vector3(4, 0.3, -16),
-    ];
+
     const curve = new THREE.CatmullRomCurve3(path, false, "catmullrom", 0.1);
-    const position = curve.getPoint(time);
-    camera.position.copy(position);
+
+    if (video) {
+      console.log(video.currentTime / video.duration);
+      if (video.currentTime / video.duration >= 1 && sceneNbr === 0) {
+        setSceneNbr(1);
+      }
+      const position = curve.getPoint(video.currentTime / video.duration);
+      camera.position.copy(position);
+      camera.lookAt(position.x, position.y, position.z);
+    }
+
     if (actions[Math.floor(elapsedTime)]) {
       actions[Math.floor(elapsedTime)]();
     }
-    // camera.lookAt(curve.getPoint((time + 0.01) % 1));
-    /*
-      const differenceX = Math.abs(camera.position.x - frameDetails.x);
-      if (camera.position.x < frameDetails.x) {
-        camera.position.x += xSpeed * differenceX;
-      } else if (camera.position.x > frameDetails.x) {
-        camera.position.x -= xSpeed * differenceX;
-      }
-      const differenceY = Math.abs(camera.position.y - frameDetails.y);
-      if (camera.position.y < frameDetails.y) {
-        camera.position.y += ySpeed * differenceY;
-      } else if (camera.position.x > frameDetails.y) {
-        camera.position.y -= ySpeed * differenceY;
-      }
-
-      const differenceZ = Math.abs(camera.position.z - frameDetails.z);
-      if (camera.position.z < frameDetails.z) {
-        camera.position.z += zSpeed * differenceZ;
-      } else if (camera.position.z > frameDetails.z) {
-        camera.position.z -= zSpeed * differenceZ;
-      }
-
-      if (
-        Math.abs(differenceZ) <= 0.1 &&
-        Math.abs(differenceZ) <= 0.1 &&
-        Math.abs(differenceZ) <= 0.1
-      ) {
-        if (currentFrame + 1 === frames[0].points.length) {
-          currentFrame = 0;
-        } else {
-          if (frames[0].actions[currentFrame]) {
-            frames[0].actions[currentFrame]();
-          }
-          currentFrame++;
-        }
-      }*/
   });
   return (
     <>
-      <Environment
-        preset="city"
-        // can be true, false or "only" (which only sets the background) (default: false)
-        backgroundBlurriness={0.07} // optional blur factor between 0 and 1 (default: 0, only works with three 0.146 and up)
-        environmentIntensity={0.005} // optional environment light intensity (default: 1)
-        backgroundIntensity={200}
-        background={true}
-      />
       <EffectComposer>
-        <Vignette eskil={false} offset={0.1} darkness={1.5} />
+        <Vignette eskil={false} offset={0.025} darkness={1.5} />
         <Bloom luminanceThreshold={0.1} mipmapBlur />
         <Noise opacity={0.02} />
       </EffectComposer>
 
-      {/*  <Perf position="top-right" />*/}
-      <group position={[5, 0.5, -7]} rotation={[0, Math.PI / 0.54, 0]}>
-        {carModel.scene && (
-          <primitive
-            object={carModel.scene}
-            castShadow={true}
-            receiveShadow={true}
+      <Perf position="top-right" />
+
+      <Environment
+        preset="forest"
+        // can be true, false or "only" (which only sets the background) (default: false)
+        environmentIntensity={0.8} // optional environment light intensity (default: 1)
+      />
+
+      {/*
+      <mesh rotation={[0, 0, 0]} position={[0, 10, -10.1]} scale={2}>
+        <planeGeometry args={[10.2, 20.2]} />
+        <meshStandardMaterial emissive={"white"} side={THREE.DoubleSide}>
+          <videoTexture
+            attach="map"
+            args={[video]}
+            colorSpace={THREE.SRGBColorSpace}
           />
-        )}
+          <videoTexture
+            attach="emissiveMap"
+            args={[video]}
+            colorSpace={THREE.SRGBColorSpace}
+          />
+        </meshStandardMaterial>
+      </mesh>
+      */}
+      <group
+        position={[0, -2.2, -5]}
+        rotation={[0, -Math.PI / 0.5, 0]}
+        scale={0.5}
+      >
+        {carModel.scene && <primitive object={carModel.scene} />}
       </group>
-      {tunnelModel.scene && (
-        <primitive
-          object={tunnelModel.scene}
-          castShadow={true}
-          receiveShadow={true}
-          scale={2}
-          position={[0, -0.7, 0]}
-          rotation={[0, Math.PI / 3, 0]}
-        />
-      )}
-      <ambientLight intensity={0.1} />
+
+      <group>
+        <mesh
+          position={[0, -2.3, 0]}
+          rotation={[-Math.PI / 2, 0, 0]}
+          scale={[1, 1, 1]}
+          receiveShadow
+        >
+          <planeGeometry args={[100, 100]} />
+          <shadowMaterial opacity={0.8} />
+        </mesh>
+      </group>
+      <ambientLight intensity={10} />
       <directionalLight
-        position={[-40, 3, -10]}
+        position={[-4, 12, -40]}
         castShadow
-        intensity={1.3}
+        intensity={0.3}
         shadow-mapSize-height={1024}
         shadow-mapSize-width={1024}
         shadow-camera-far={50}
         shadow-camera-left={-10}
-        color={0x66aaff}
+        color={"white"}
       />
 
       <OrbitControls />
